77bc9ff9511fd9ca0d3cbe80cc3538b4f5e9ece6
SiddharthaBhattacharjee <siddharthabhatt2345@gmail.com>
2022-11-13 12:19:31 +0530

    Initialized

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>option1</div>
+            <div>option1</div>
+            <div>option1</div>
+            <div>option1</div>
+            <div>option1</div>
+        </nav>
+    );
+};
+
+export default Navbar;
