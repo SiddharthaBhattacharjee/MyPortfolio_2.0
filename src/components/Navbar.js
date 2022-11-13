15a74bc34dd8b5afee04871db8c07764fd899f65
SiddharthaBhattacharjee <siddharthabhatt2345@gmail.com>
2022-11-13 13:03:17 +0530

    NavBar Started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Navbar.css";
+import dp from '../images/dp.jpeg';
 
 const Navbar = () => {
     return(
         <nav>
-            <div>option1</div>
-            <div>option1</div>
-            <div>option1</div>
-            <div>option1</div>
-            <div>option1</div>
+            <div id="logoContainer">
+                <img src={dp} alt="profileIcon"></img>
+            </div>
+            <div id="options">
+                <div>option1</div>
+                <div>option1</div>
+                <div>option1</div>
+                <div>option1</div>
+                <div>option1</div>
+            </div>
+            
         </nav>
     );
 };
